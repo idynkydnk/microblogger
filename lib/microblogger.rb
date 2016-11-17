b590d3bb7e9c0d6bb055cb14452ccb34f4a41496
Kyle Thomson <idynkydnk@gmail.com>
2016-11-16 19:26:10 -0500

    finished everyones last tweet method

--- lib/microblogger.rb
+++ lib/microblogger.rb
@@ -17,7 +17,7 @@ class MicroBlogger
     puts "Welcome to Kyle's Twitter Client!"
     command = ""
     while command != "q"
-      printf "Enter command: t,q,dm:  "
+      printf "Enter command: t,q,dm,spam,elt:  "
       input = gets.chomp
       parts = input.split(" ")
       command = parts[0]
@@ -31,6 +31,8 @@ class MicroBlogger
           puts "Enter a tweet after the letter 't' or maybe you just entered more than 140 characters"
         end
       when 'dm' then dm(parts[1], parts[2..-1].join(" "))
+      when 'spam' then spam_my_followers(parts[1..-1].join(" "))
+      when 'elt' then everyones_last_tweet
       else
         puts "Sorry, don't know that command"
       end
@@ -42,10 +44,42 @@ class MicroBlogger
   end
   
   def dm(target, message)
-    puts "Sending #{target} this direct message:"
-    puts message
-    message = "d @#{target} #{message}"
-    tweet(message)
+    screen_names = @client.followers.collect { |follower|
+      @client.user(follower).screen_name }
+    if screen_names.include?(target)
+      puts "Sending #{target} this direct message:"
+      puts message
+      message = "d @#{target} #{message}"
+      tweet(message)
+    else
+      puts "You're not even friends with that user!"
+    end
+  end
+
+  def followers_list
+    screen_names = []
+    @client.followers.each do |follower|
+      screen_names << @client.user(follower).screen_name
+    end
+    return screen_names
+  end
+  
+  def spam_my_followers(message)
+    followers_list.each do |follower|
+      dm(follower, message)
+    end
+  end
+
+  def everyones_last_tweet
+    friends = @client.friends.sort_by { |friend| friend.screen_name.downcase}
+    friends.each do |friend|
+      timestamp = friend.status.created_at
+      puts friend.screen_name + " tweeted this on " + 
+        timestamp.strftime("%A, %b %d")
+      puts friend.status.full_text
+      puts
+      puts
+    end
   end
 end
 
